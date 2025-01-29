cee84382aa8ba64d6e9930c403847879cd13be00
Vinicius da Silva <138536413+Vinii-Silva@users.noreply.github.com>
2025-01-28 21:46:17 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+const botaoSim = document.getElementById('sim');
+
+botaoSim.addEventListener('click', () => {
+  window.location.href = 'https://www.youtube.com/watch?v=rC6r1ne-BZ0';
+});
